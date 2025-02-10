18106e02f6f4f0508bce34fb55edfced71f2b86d
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-02-10 13:58:31 +0100

    linkedlist practice

--- linkedlist.c
+++ linkedlist.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+//Did this tutorial for practice
+//https://www.youtube.com/watch?v=VOpjAHCee7c
+
+typedef struct node{
     int value;
     struct node* next;
-};
-
-typedef struct node node_t;
+}node_t;
 
 void printlist (node_t *head){
     node_t *temporary = head;
@@ -16,24 +17,50 @@ void printlist (node_t *head){
         temporary = temporary->next;
     }
     printf("\n");
-
-
 }
 
+node_t *create_new_node(int value){
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+}
+
+node_t *insert_at_head(node_t **head, node_t *node_to_insert){
+        node_to_insert->next = *head;
+        *head = node_to_insert;
+        return node_to_insert;
+    }
+
+node_t *find_node(node_t *head, int value){
+    node_t *tmp = head;
+    while(tmp != NULL){
+        if(tmp->value == value)
+            return tmp;
+            tmp = tmp->next;   
+    }
+    return NULL;
+}
+
+void *insert_after_node(node_t *node_insert_after_node, node_t* new_node){
+    new_node->next = node_insert_after_node->next;
+    node_insert_after_node->next = new_node;
+}
 
 int main(){
 
-    node_t n1, n2, n3;
-    node_t *head;
+    node_t *head = NULL;
+    node_t *tmp;
 
-    n1.value = 45;
-    n2.value =  8;
-    n3.value = 32;
+    for(int i = 0; i < 25; i++){
+        tmp = create_new_node(i);
+        insert_at_head(&head, tmp);
+    }
 
-    head = &n3;
-    n3.next = &n2;
-    n2.next = &n1;
-    n1.next = NULL;
+    tmp = find_node(head, 13);
+    printf("found node with value %d\n",tmp->value);
+
+    insert_after_node(tmp, create_new_node(75));
 
     printlist(head);
 
