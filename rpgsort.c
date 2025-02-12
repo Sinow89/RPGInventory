9aaea597ee2f2e1af6da01484d41da78f9796bf7
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-02-12 11:47:47 +0100

    Added a super simple hash table function for prototyping.

--- rpgsort.c
+++ rpgsort.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "raylib.h"
 #include "raymath.h"
 
 #define TOTAL_ITEMS 10
+#define MAX_NAME 255
 
 typedef struct item_t{
-    char name[20];
+    char name[MAX_NAME];
     int gold;
     int rarity;
     float kilo;
     int received_order;
 } item_t;
+
+unsigned int hash(char *name){
+    int length = strnlen(name, MAX_NAME);
+    unsigned int hash_value = 0;
+    for (int i = 0; i < length; i++){
+        hash_value += name[i];
+    }
+    return hash_value;
+}
 
 void insertionSort(item_t arr[], int n)
 {
@@ -66,18 +78,22 @@ int main(){
         {"Platinum Sword", 5000, 5, 50.0f, 7}
     };
 
+    printf("Dagger=> %u\n", hash(item[0].name));
+    printf("Sword=> %u\n", hash(item[1].name));
+    printf("Armor=> %u\n", hash(item[2].name));
+
     while(!WindowShouldClose()){
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
 
         char item_text[256];
 
         // Format the debug information into a string
         for(int i = 0; i < TOTAL_ITEMS; i++){
             sprintf(item_text, "Name - Gold - Rarity - Kilo - Newest");
-            DrawText(item_text, 200, 50, 20, BLACK);
+            DrawText(item_text, 200, 50, 20, WHITE);
             sprintf(item_text, "Press S to sort Newest and again for Oldest");
-            DrawText(item_text, 200, 500, 20, BLACK);
+            DrawText(item_text, 200, 500, 20, DARKGRAY);
             sprintf(item_text, "Item: %s, %.2d, %d, %.2f, %d", item[i].name, item[i].gold, item[i].rarity, item[i].kilo, item[i].received_order);
             DrawText(item_text, 200, 100 + (i * 30), 20, DARKGRAY);
         }
